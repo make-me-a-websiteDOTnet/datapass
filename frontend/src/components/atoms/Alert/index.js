8ae547c2b72b0c4f3cf7b385f3bfd60d6bf3ab57
Isabelle Lafont <isalafont@gmail.com>
2022-08-25 14:23:05 +0200

    Add button to alert component

--- frontend/src/components/atoms/Alert/index.js
+++ frontend/src/components/atoms/Alert/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './style.css';
 
-export const Alert = ({ type = 'info', title, children }) => (
+export const Alert = ({
+  type = 'info',
+  title,
+  children,
+  onAlertClose = null,
+}) => (
   <div role="alert" className={`fr-alert fr-alert--${type}`}>
     {title && <p className="fr-alert__title">{title}</p>}
     {children}
+    {onAlertClose && (
+      <button class="fr-link--close fr-link" onClick={onAlertClose}>
+        Masquer le message
+      </button>
+    )}
   </div>
 );
 
